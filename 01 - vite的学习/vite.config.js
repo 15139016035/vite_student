f7699bfbd5b860342c2f648c7dfd78c2f13efbf7
wuyaohua <460615310@qq.com>
2025-09-01 03:06:41 +0800

    依赖预构建

--- 01 - vite的学习/vite.config.js
+++ 01 - vite的学习/vite.config.js
@@ -2,6 +2,6 @@ export default {
      optimization: {
          //语雀：02- vite初体验和依赖预构建 ，exclude: ["lodash"],
          //当遇到lodash依赖的时候，不进行依赖预构建, 就会导致引入lodash后，又请求一大堆js资源都是lodash引用的依赖；
-         exclude: ["lodash"], 
+         exclude: ["lodash"],
      }
 }
